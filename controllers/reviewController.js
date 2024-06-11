3c0d5ab4b6323487e40048ac28409e4de49bb362
Tamanna bhagat <tamanna.bhagat2826@gmail.com>
2024-06-11 19:57:06 +0530

    code pushed with getReview API

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -58,4 +58,53 @@ const getProductReviews = async (req, res) => {
     });
   }
 };
-module.exports = { addOrUpdateReview,getProductReviews}
+const getUserProductReviews = async (req, res) => {
+  const { user_id,product_ids } = req.body; 
+
+  try {
+    const reviews = await Review.findAll({
+      where: {
+        user_id,
+        product_id: product_ids,
+      },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["first_name", "last_name"],
+        },
+      ],
+    });
+
+    // Create a map of product_id to review
+    const reviewsMap = reviews.reduce((acc, review) => {
+      acc[review.product_id] = review;
+      return acc;
+    }, {});
+
+    // Create a response for each product_id
+    const response = product_ids.map(product_id => {
+      if (reviewsMap[product_id]) {
+        return {
+          product_id,
+          review: reviewsMap[product_id],
+        };
+      } else {
+        return {
+          product_id,
+          review: "No review found",
+        };
+      }
+    });
+
+    res.status(200).json({ success: true, reviews: response });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching the reviews",
+      error,
+    });
+  }
+};
+
+module.exports = { addOrUpdateReview, getProductReviews, getUserProductReviews };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addOrUpdateReview, getProductReviews } = require('../controllers/reviewController');
+const { addOrUpdateReview, getProductReviews,getUserProductReviews } = require('../controllers/reviewController');
 
 router.post('/add', addOrUpdateReview);
 router.get('/product/:product_id', getProductReviews);
+router.post('/user-product-reviews', getUserProductReviews);
 
 module.exports = router;
